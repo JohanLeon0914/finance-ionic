0f1b3e3c418cce993b9f8f0bd47190b9dba5cb0f
Johan León <64605783+JohanLeon0914@users.noreply.github.com>
2024-07-08 14:48:59 -0500

    agregando opcion de seleccionar icono en categorias

--- src/app/pages/categories/categories.page.ts
+++ src/app/pages/categories/categories.page.ts
@@ -1,9 +1,10 @@
+import { NgClass } from '@angular/common';
 import { Component, OnInit, ViewChild, inject } from '@angular/core';
 import { FormsModule, NgForm, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { IonModal, IonHeader, IonIcon, IonToolbar, IonTitle, IonContent, IonButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonList, IonItem, IonSelect, IonButtons, IonInput, IonSelectOption, IonLabel, IonCheckbox, IonText } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { alertCircleOutline, checkmarkCircleOutline, closeCircleOutline, pencilOutline, pricetagOutline, trashOutline } from 'ionicons/icons';
+import { pricetagOutline, closeCircleOutline, checkmarkCircleOutline, alertCircleOutline, pencilOutline, trashOutline, addCircleOutline, airplaneOutline, alarmOutline, albumsOutline, analyticsOutline, apertureOutline, appsOutline, archiveOutline, arrowBackCircleOutline, arrowDownCircleOutline, arrowForwardCircleOutline, arrowUpCircleOutline, atCircleOutline, bagOutline, balloonOutline, barChartOutline, barcodeOutline, basketOutline, batteryChargingOutline, batteryDeadOutline, batteryFullOutline, beakerOutline, bedOutline, beerOutline, bicycleOutline, bluetoothOutline, boatOutline, bookOutline, bookmarkOutline, briefcaseOutline, brushOutline, bugOutline, buildOutline, bulbOutline, busOutline, businessOutline, cafeOutline, calculatorOutline, calendarOutline, callOutline, cameraOutline, carOutline, cardOutline, cartOutline, cashOutline, chatbubbleOutline, checkmarkDoneOutline, cloudDownloadOutline, cloudUploadOutline } from 'ionicons/icons';
 import { Subscription } from 'rxjs';
 import { AuthService } from 'src/app/services/auth.service';
 import { UtilService } from 'src/app/services/utils.service';
@@ -17,7 +18,7 @@ import { Wallet } from 'src/models/wallet.model';
   templateUrl: 'categories.page.html',
   styleUrls: ['categories.page.scss'],
   standalone: true,
-  imports: [IonText, IonCheckbox, IonLabel, FormsModule, ReactiveFormsModule, IonInput, IonButtons, IonModal, IonCardContent, IonIcon, IonHeader, IonToolbar, IonTitle, IonContent, HeaderComponent, IonButton, IonCard, IonCardHeader, IonCardTitle, IonList, IonItem, IonSelect, IonSelectOption],
+  imports: [NgClass, IonIcon ,IonText, IonCheckbox, IonLabel, FormsModule, ReactiveFormsModule, IonInput, IonButtons, IonModal, IonCardContent, IonIcon, IonHeader, IonToolbar, IonTitle, IonContent, HeaderComponent, IonButton, IonCard, IonCardHeader, IonCardTitle, IonList, IonItem, IonSelect, IonSelectOption],
 })
 export class CategoriesPage implements OnInit {
 
@@ -31,9 +32,76 @@ export class CategoriesPage implements OnInit {
   categories: Category[] = [];
   categorySelected: Category | null = null;
   categoryName: string = '';
-
+  categoryIcon: string = '';
+  iconNames: string[] = [
+    'pricetag-outline',
+    'close-circle-outline',
+    'checkmark-circle-outline',
+    'alert-circle-outline',
+    'pencil-outline',
+    'trash-outline',
+    'add-circle-outline',
+    'airplane-outline',
+    'alarm-outline',
+    'albums-outline',
+    'analytics-outline',
+    'aperture-outline',
+    'apps-outline',
+    'archive-outline',
+    'arrow-back-circle-outline',
+    'arrow-down-circle-outline',
+    'arrow-forward-circle-outline',
+    'arrow-up-circle-outline',
+    'at-circle-outline',
+    'bag-outline',
+    'balloon-outline',
+    'bar-chart-outline',
+    'barcode-outline',
+    'basket-outline',
+    'battery-charging-outline',
+    'battery-dead-outline',
+    'battery-full-outline',
+    'beaker-outline',
+    'bed-outline',
+    'beer-outline',
+    'bicycle-outline',
+    'bluetooth-outline',
+    'boat-outline',
+    'book-outline',
+    'bookmark-outline',
+    'briefcase-outline',
+    'brush-outline',
+    'bug-outline',
+    'build-outline',
+    'bulb-outline',
+    'bus-outline',
+    'business-outline',
+    'cafe-outline',
+    'calculator-outline',
+    'calendar-outline',
+    'call-outline',
+    'camera-outline',
+    'car-outline',
+    'card-outline',
+    'cart-outline',
+    'cash-outline',
+    'chatbubble-outline',
+    'checkmark-done-outline',
+    'cloud-download-outline',
+    'cloud-upload-outline'
+  ];
   constructor() {
-    addIcons({ pricetagOutline, closeCircleOutline, checkmarkCircleOutline, alertCircleOutline, pencilOutline, trashOutline })
+    addIcons({
+      pricetagOutline, closeCircleOutline, checkmarkCircleOutline, alertCircleOutline, pencilOutline, trashOutline,
+      addCircleOutline, airplaneOutline, alarmOutline, albumsOutline, analyticsOutline, apertureOutline, appsOutline, 
+      archiveOutline, arrowBackCircleOutline, arrowDownCircleOutline, arrowForwardCircleOutline, arrowUpCircleOutline, 
+      atCircleOutline, bagOutline, balloonOutline, barChartOutline, barcodeOutline, basketOutline, batteryChargingOutline, 
+      batteryDeadOutline, batteryFullOutline, beakerOutline, bedOutline, beerOutline, bicycleOutline, bluetoothOutline, 
+      boatOutline, bookOutline, bookmarkOutline, briefcaseOutline, brushOutline, bugOutline, buildOutline, bulbOutline, 
+      busOutline, businessOutline, cafeOutline, calculatorOutline, calendarOutline, callOutline, cameraOutline, 
+      carOutline, cardOutline, cartOutline, cashOutline, chatbubbleOutline, checkmarkDoneOutline, cloudDownloadOutline, 
+      cloudUploadOutline
+    });
   }
 
   ngOnInit(): void {
@@ -80,15 +148,19 @@ export class CategoriesPage implements OnInit {
       if(this.categorySelected) {
         const category: Category = {
           name: this.categorySelected.name,
+          icon: this.categoryIcon,
           id: this.categorySelected.id
         } 
         this.createOrUpdateCategory(category, false, form);
       } else {
         const category: Category = {
-          name: this.categoryName
+          name: this.categoryName,
+          icon: this.categoryIcon
         } 
         this.createOrUpdateCategory(category, true, form);
       }
+      this.categoryName = '';
+      this.categoryIcon = '';
     }
   }
 
@@ -122,6 +194,11 @@ export class CategoriesPage implements OnInit {
       }
     )
   }
+
+  onSelectIcon(icon: string) {
+    this.categoryIcon = icon;
+  }
+
   cancel() {
     this.categoryName = '';
     this.modal.dismiss(null, 'cancel');
